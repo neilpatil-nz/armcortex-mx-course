e516b87332a631e5e946847c576592306e1356de
Neil Patil <npat692@aucklanduni.ac.nz>
2021-11-11 13:01:19 +1300

    ARM Processor and semihosting

---
+++ workspace/001HelloWorld/Debug/Src/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F303VCTx -DSTM32 -DSTM32F3 -DSTM32F3DISCOVERY -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
